34c8cddaedb59dbd2d57783c91849e345a5b6030
eAlasdair <25916475+eAlasdair@users.noreply.github.com>
2022-12-03 13:35:18 +1300

    initial commit

---
+++ Main.cs
@@ -0,0 +1,49 @@
+﻿using UnityModManagerNet;
+using HarmonyLib;
+using DV;
+
+namespace FreeCabooseMod
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry entry;
+
+        /**
+         * Mod entrypoint
+         * Returns true if load is successful
+         */
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            entry = modEntry;
+
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+
+            modEntry.OnUnload = OnUnload;
+
+            return true;
+        }
+
+        /**
+         * Unpatch the mod (hopefully) safely
+         */
+        private static bool OnUnload(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+
+        /**
+         * Set the summon price of all crew vehicles to 0
+         */
+        [HarmonyPatch(typeof(CommsRadioCrewVehicle), "SummonPrice", MethodType.Getter)]
+        public static class SummonPricePatch
+        {
+            static void Postfix(ref float __result)
+            {
+                __result = 0f;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Free Caboose
+
+This is a mod for the video game Derail Valley created by Altfuture.  
+It sets the price of summoning all crew vehicles to 0
+
+Download: [Nexus Mods]()  
+Install using [Unity Mod Manager]()
